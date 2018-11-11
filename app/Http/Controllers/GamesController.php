7f69d37eee7cb4e9ead82e5644da2ba88c32a59c
briantmorr <briantmorr@gmail.com>
2018-11-10 18:22:09 -0800

    removed garbage

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -17,43 +17,16 @@ class GamesController extends Controller
       return response()->json($results);
     }
     public function leagues(){
-      // $client = new Client(['headers' => ['X-Auth-Token' => env('FOOTBALL_DATA_API_KEY')]]);
-      // $client = new Client();
-      // ['defaults' => [
-      //   'headers' => ['X-Auth-Token' => env('FOOTBALL_DATA_API_KEY')]
-      // ]]);
 
         //THIS WORKS
     $client = new \GuzzleHttp\Client();
     $headers = ['headers' => ['X-Auth-Token' => '643ecaa8ac1e48eea0a07dd29ad2a910']];
     $res = $client->get('https://api.football-data.org/v2/competitions?plan=TIER_ONE', $headers);
-    // print_r($res->getBody());
-    // echo $res->getBody();
     $json = json_decode($res->getBody());
-    // ['auth' =>  ['user', 'pass']]);
-    // echo $res->getStatusCode(); // 20
-    // echo $res->json();
-    // echo $res->getBody();
-    // $result = json_decode($res->getBody(), true);
-    // print_r($result, true);
-    // echo $result->competitions;
-
-      // $response = $client->get('https://api.football-data.org/v2/competitions?plan=TIER_ONE');
-      // return $response;
-
-      // $uri = 'http://api.football-data.org/v2/competitions?plan=TIER_ONE';
-      // $reqPrefs['http']['method'] = 'GET';
-      // $reqPrefs['http']['header'] = "X-Auth-Token: {$_ENV['FOOTBALL_DATA_API_KEY']}";
-      // $stream_context = stream_context_create($reqPrefs);
-      // $response = file_get_contents($uri, false, $stream_context);
-      // $result = json_decode($response);
-      // echo count($res);
+    
 
 
       foreach ($json->competitions as $key => $value) {
-        // echo $key;
-        // echo "\n";
-        // echo "\n";
 
         $league = new Leagues;
         $league->competition_id = $value->id;
@@ -63,6 +36,7 @@ class GamesController extends Controller
         $league->competition_code = $value->code;
 
         $league->save();
+        //raw sql query that does the same thing
         // return DB::insert("INSERT INTO leagues (competition_id, area_id, area_name, competition_name, competition_code) VALUES ($value->id, $value->area->id, $value->area->name, $value->name, $value->code)");
       }
       $strMessage =  "successfully inserted " . count($json->competitions) ." records into database";
