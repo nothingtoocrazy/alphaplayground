b02ce7dd0fc21554c771bc0e680af409ba63145f
briantmorr <briantmorr@gmail.com>
2018-12-02 14:20:25 -0800

    created basic matches route

---
+++ app/Http/Controllers/MatchesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MatchesController extends Controller
+{
+    // public function users(){
+    // $userModel = Users::all();
+    // print_r($userModel);
+    // foreach($userModel as $user){
+    //     echo $user;
+    // }
+    public function matches() {
+        return "it worked";
+    }
+    
+}
+
+
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,9 +7,6 @@ use App\Models\Users;
 
 class UserController extends Controller
 {
-    /**
-     * z
-     */
     public function users(){
     $userModel = Users::all();
     print_r($userModel);
