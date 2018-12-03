967182e57a678eb7e8b1188d3c5faba04e40c3a8
Taylor Sturtz <taylor.sturtz@ytel.com>
2018-12-02 20:48:04 -0800

    replaced dummy data with matches data from DB and added count to payload

--- app/Http/Controllers/LeagueController.php
+++ app/Http/Controllers/LeagueController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use DB;
 use App\Models\Leagues;
 
 class LeagueController extends Controller
@@ -12,7 +11,7 @@ class LeagueController extends Controller
      * get request to display all leagues of given type 
  
      * @param $league the league that we will get from the user to display (e.g. Premier, Bundasliga) 
-     * */ 
+     * */
        
     public function league($league = null) {
       $results = Leagues::all();
--- app/Http/Controllers/MatchesController.php
+++ app/Http/Controllers/MatchesController.php
@@ -3,36 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Matches;
 
-class MatchesController extends Controller
-{
-    // public function users(){
-    // $userModel = Users::all();
-    // print_r($userModel);
-    // foreach($userModel as $user){
-    //     echo $user;
-    // }
-    
-    public function matches() {
-        // $dummyMatches = new \stdClass();
-        // $dummyMatches->matches = [];
-        $dummyMatches = [];
-        $match = new \stdClass();
-        $match->title = "Tottenham vs. Arsenal";
-        $match->score = "1-0";
-        $dummyMatches[] = $match;
-        $match = new \stdClass();
-        $match->title = "Liverpool vs. Everton";
-        $match->score = "1-4";
-        $dummyMatches[] = $match;
-       
-        
-        
-        
-        return json_encode($dummyMatches);
-        //"it worked";
-    }
-    
+class MatchesController extends Controller {
+  public function matches() {
+    $results = Matches::all();
+    $payload = array(
+      'count' => count($results),
+      'matches' => $results
+    );
+    return $payload;
+  }
 }
-
-
